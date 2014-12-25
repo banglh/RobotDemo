d3b42453838cf0b1664ae0b8de3af421da2372c0
banglh <ice.banglh@gmail.com>
2014-12-25 21:57:44 +0900

    add GameState library

---
+++ GameState.c
@@ -0,0 +1,19 @@
+#include "GameState.h"
+
+GameState newGameState(Position rbP, Position hmP) {
+    GameState gs;
+    gs.rbPos = rbP;
+    gs.hmPos = hmP;
+    return gs;
+}
+
+void setGameState(GameState * gs, Position rbP, Position hmP) {
+    (*gs).rbPos = rbP;
+    (*gs).hmPos = hmP;
+}
+
+int isSameGameState(GameState gs1, GameState gs2) {
+    if (isSamePos2(gs1.rbPos, gs2.rbPos) && isSamePos2(gs1.hmPos, gs2.hmPos))
+        return TRUE;
+    return FALSE;
+}
---
+++ GameState.h
@@ -0,0 +1,17 @@
+#ifndef __GAME_STATE_H
+#define __GAME_STATE_H
+
+#include <stdio.h>
+#include "Constants.h"
+#include "Position.h"
+
+typedef struct {
+    Position rbPos;
+    Position hmPos;
+} GameState;
+
+GameState newGameState(Position rbP, Position hmP);
+void setGameState(GameState * gs, Position rbP, Position hmP);
+int isSameGameState(GameState gs1, GameState gs2);
+
+#endif // __GAME_STATE_H
